ef43e65dbf3fbd7b86b3df6a6f442d1db1c7c03b
IshaBP <isha.pachkawade11@gmail.com>
2020-12-28 11:39:13 +0530

    Done: NavBar

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -21,40 +21,47 @@ const Navigation: React.FunctionComponent<NavigationProps> = ({
   const { isDarkModeActive } = React.useContext(ThemeContext);
   const [activeMenuItem, setActiveMenuItem] = useState(navItems[0].id);
   const [isVisibleMobileMenu, setVisibleMobileMenu] = useState(false);
-  let modeClass = isDarkModeActive ? "dark" : "light";
+  const MobileMenuIcon = isVisibleMobileMenu ? IoClose : FaBars;
+  const modeClass = isDarkModeActive ? "dark" : "light";
+  const navOpenClass = isVisibleMobileMenu ? "Expanded" : "";
 
-  return (
-    // <div className={`${modeClass}`}>
-    <div className="nav">
-      <Link to="/" className="logoContainer">
-        IP
-      </Link>
-
-      {isVisibleMobileMenu ? (
-        <div className="mobileIconContainer">
-          <IoClose onClick={() => setVisibleMobileMenu(false)} />
-        </div>
-      ) : (
-        <div className="mobileIconContainer">
-          <FaBars onClick={() => setVisibleMobileMenu(true)} />
-        </div>
-      )}
-
-      <div className={"navMenu"}>
-        {navItems.map((item, index) => (
-          <div key={item.section}>
+  const renderNavLinks = () => {
+    return (
+      <div className={`navMenu${navOpenClass}`}>
+        {navItems.map(item => (
+          <div key={item.section} className="navLinkContainer">
             <Link
               to={item.path}
               className={`navLink ${
                 item.id === activeMenuItem ? "active" : ""
               }`}
-              onClick={() => setActiveMenuItem(item.id)}
+              onClick={() => {
+                setActiveMenuItem(item.id);
+                setVisibleMobileMenu(!isVisibleMobileMenu);
+              }}
             >
               {item.section}
             </Link>
           </div>
         ))}
       </div>
+    );
+  };
+
+  return (
+    <div
+      className={`navContainer nav${navOpenClass}`} /* className={`${modeClass}`} */
+    >
+      <Link to="/" className="logoContainer">
+        IP
+      </Link>
+
+      <MobileMenuIcon
+        onClick={() => setVisibleMobileMenu(!isVisibleMobileMenu)}
+        className="mobileIconContainer"
+      />
+
+      {renderNavLinks()}
     </div>
   );
 };
