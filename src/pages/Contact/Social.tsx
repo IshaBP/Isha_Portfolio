ebe6e4bc85b85438613fe27c19c0642df1b90552
IshaBP <isha.pachkawade11@gmail.com>
2021-01-03 19:51:01 +0530

    Added files

---
+++ src/pages/Contact/Social.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Contact = () => {
+  return <div className="section-inner">Contact ISHA PACHKAWADE</div>;
+};
+
+export default Contact;
---
+++ src/pages/Skills/Tools.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Technologies = () => {
+  return <div className="section-inner">Technologies ISHA PACHKAWADE</div>;
+};
+
+export default Technologies;
