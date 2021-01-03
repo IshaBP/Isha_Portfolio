9a4f098d1618f49f972a5445eadeec9f926715f6
IshaBP <isha.pachkawade11@gmail.com>
2021-01-03 19:22:49 +0530

    wip: About

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import { Timeline } from '../../components/TimeLine/TimeLine';
+import { TIMELINE_DATA } from './constants';
+
 const About = () => {
-  return <div className="section-inner">About ISHA PACHKAWADE</div>;
+  return (
+    <div className="section-inner">
+      <Timeline timelineData={TIMELINE_DATA} />
+    </div>
+  );
 };
 
 export default About;
